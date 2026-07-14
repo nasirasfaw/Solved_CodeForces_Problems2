dfd4dbb140c3e6ef8feaf4595825bfb7962d52bf
Nasir Asfaw <nasirasfawk@gmail.com>
2026-07-15 00:36:39 +0300

    2229B - Absolute Cinema

---
+++ 2229B.py
@@ -0,0 +1,13 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+
+    for i in range(n):
+        if a[i] > b[i]:
+            a[i], b[i] = b[i], a[i]
+
+    answer = max(a) + sum(b)
+
+    print(answer)
