ac656c3feb46deced58f60c19483cee690e09f6c
Nasir Asfaw <nasirasfawk@gmail.com>
2026-07-21 22:49:23 +0300

    1327A - Sum of Odd Integers

---
+++ 1327A.py
@@ -0,0 +1,10 @@
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+
+    if n >= k**2 and n % 2 == k % 2:
+        answer = "YES"
+    else:
+        answer = "NO"
+
+    print(answer)
